7ac294e19b81a2092fa3db5704876ceea37c2e9e
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-07-21 22:26:07 -0300

    starting project

---
+++ script/conversor.js
@@ -0,0 +1,19 @@
+function conversor(p1, p2) {
+  return p1 * p2;
+  const moeda = document.getElementsById("reais").value
+}
+
+
+
+
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
+document.getElementById("demo").innerHTML = toCelsius(77);
+
+function kiloweightConvert(value) {
+  document.getElementById("Pounds").innerHTML=value*2.2046;
+  document.getElementById("Ounces").innerHTML=value*35.274;
+  document.getElementById("Grams").innerHTML=value*1000;
+  document.getElementById("Stones").innerHTML=value*0.1574;
+}
