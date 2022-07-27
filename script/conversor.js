0e1163a2886742c51bf7c458f1deda084d4ab1d0
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-07-26 22:33:53 -0300

    correction in function

--- script/conversor.js
+++ script/conversor.js
@@ -1,15 +1,15 @@
-const MoedaReal = document.querySelector("#MoedaReal").value
-
-const btn-converter = document.querySelector("#btnConverter").value
-
-button.onclick = () =>
-
-switch (converter) {
-  case 'dolar': 
-  BRL * 5,5 BRL;
-  break;
-  default:
-    btn-converter.innerHTML =
-
-
+function MoedaReal(num1, num2){
+  n1 = parseFloat(document.getElementsById("n1").value);
+  n1 = parseFloat(document.getElementsById("n2").value);
 }
+
+selector = document.getElementsById("selector").value
+
+switch (selector) {
+  case 'dólar':
+        calculo = (n1 / n2)
+        document.getElementById("resultado").innerHTML = `O valor de ${n1} em dólar é = ${calculo}`
+        break
+}
+
+
