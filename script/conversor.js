8e397ac329eaae5be1e9484d823cd9385ab0e536
AdrianoFront <adrianolefou@gmail.com>
2022-11-12 21:42:47 -0300

    Criando função de click e evento de botão

--- script/conversor.js
+++ script/conversor.js
@@ -1,1 +1,10 @@
-document.getElementById
+const button = document.getElementsByClassName('convert-button')
+
+const dolar = 5.4
+
+const convertValues = () =>{
+    const input = document.getElementById('input-real').value
+    inputReais / dolar
+}
+
+button.addEventListner('click', convertValues)
