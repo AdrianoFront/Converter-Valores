736abcc58f9481247db38f2bf169effc3bba62dd
AdrianoFront <adrianolefou@gmail.com>
2022-11-14 22:23:03 -0300

    Formatado número natural para valor monetário

--- script/conversor.js
+++ script/conversor.js
@@ -6,9 +6,18 @@ const convertValues = () =>{
     const inputReais = document.getElementById('input-real').value
     const realValue = document.getElementById('real-value')
     const currencyValue = document.getElementById('currency-value')
-    
-    realValue.innerHTML = inputReais
-    currencyValue.innerHTML = inputReais / dolar
+        
+    currencyValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency:"BRL",
+    }).format(inputReais);
+
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency:"USD",
+    }).format(inputReais / dolar);
+
+
 }
 
 button.addEventListner('click', convertValues)
